4ed77566a089bc05da6154d93b99c1b8b38f8f8b
Cami-13 <camselacosta@gmail.com>
2025-04-30 02:06:02 -0300

    "Menu" agregado al grupo "blocks"

--- ejercicio_senales/scenes/Buttons/Reset/reset.gd
+++ ejercicio_senales/scenes/Buttons/Reset/reset.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 
 func _on_button_down() -> void:
-	var block = get_tree().get_nodes_in_group("blocks")
+	var blocks = get_tree().get_nodes_in_group("blocks")
 	for block in blocks:
 		block.delete()
