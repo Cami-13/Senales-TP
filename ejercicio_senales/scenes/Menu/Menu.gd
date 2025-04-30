c8dc08d72c8f2826da5cf87bbe6dcd96e0f2fba4
Cami-13 <camselacosta@gmail.com>
2025-04-30 00:38:54 -0300

    Escenas conectadas con el botón

--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -13,3 +13,7 @@ func _ready():
 
 func _on_red_generator_button_down() -> void:
 	pass # Replace with function body.
+
+
+func _on_violet_generator_button_down() -> void:
+	pass # Replace with function body.
