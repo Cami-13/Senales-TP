5f56b7bc357aa5645b7b3f8469f4f38fd5b3740a
Cami-13 <camselacosta@gmail.com>
2025-04-30 00:30:45 -0300

    Kickoff

---
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const INSTANCES_TEXT = "Cantidad de instancias: "
+# Variable para contar cantidad de instancias
+var instancesCount = 0
+
+func _ready():
+	# Inicializa el texto de "CountLabel" usando el texto
+	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	pass
+
+
+func _on_red_generator_button_down() -> void:
+	pass # Replace with function body.
